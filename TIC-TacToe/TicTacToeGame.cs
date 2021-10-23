7b92c2dab53846eb77e3b8356175471aa2c68584
Tejassonar <sonartejas908@gmail.com>
2021-10-23 10:38:22 +0530

    Added User Input Method

--- TIC-TacToe/TicTacToeGame.cs
+++ TIC-TacToe/TicTacToeGame.cs
@@ -7,18 +7,34 @@ namespace TIC_TacToe
     class TicTacToeGame
     {
 
-        char[] board = new char[10];
 
         public static void GameBoard()
         {
-            Console.WriteLine("  |  |  "  );
-            Console.WriteLine("--+--+--"  );
-            Console.WriteLine("  |  |  "  );
-            Console.WriteLine("--+--+--"  );
-            Console.WriteLine("  |  |  "  );
 
+            char[] board = new char[10];
+            int n = board.Length;
+            for (int i = 1; i < n; i++)
+            {
+                board[i] = ' ';
+            }
+        }
 
-            
+        public static void UserInput()
+        {
+            Console.WriteLine(" Player1.Please Choose X or O");
+
+            char Player1 = Convert.ToChar(Console.ReadLine());
+            if(Player1 == 'X')
+            {
+                char player2 = 'O';
+                Console.WriteLine("Player1 is {0} and Player2 is {1}",Player1,player2);
+
+            }
+            else
+            {
+                char player2 = 'X';
+                Console.WriteLine("Player1 is {0} and Player2 is {1}",Player1,player2);
+            }
         }
             
     }
