8516e6057f0b5dfa64e70dc62bc0766c4d16938a
Tejassonar <sonartejas908@gmail.com>
2021-10-23 09:34:12 +0530

    Added Game Board and Char Array

---
+++ TIC-TacToe/TicTacToeGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TIC_TacToe
+{
+    class TicTacToeGame
+    {
+
+        char[] board = new char[10];
+
+        public static void GameBoard()
+        {
+            Console.WriteLine("  |  |  "  );
+            Console.WriteLine("--+--+--"  );
+            Console.WriteLine("  |  |  "  );
+            Console.WriteLine("--+--+--"  );
+            Console.WriteLine("  |  |  "  );
+
+
+            
+        }
+            
+    }
+}
